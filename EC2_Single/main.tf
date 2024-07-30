e4224331ba8b81a5f8a96b4035011c290f92a6ce
tejashjoshi11 <tejash.joshi@hotmail.com>
2024-07-30 16:56:46 +0530

    Update main.tf

--- EC2_Single/main.tf
+++ EC2_Single/main.tf
@@ -1,16 +1,9 @@
-module "ec2_instance" {
-  source  = "terraform-aws-modules/ec2-instance/aws"
-
-  name = "single-instance"
-
-  instance_type          = "t2.micro"
-  key_name               = "user1"
-  monitoring             = true
-  vpc_security_group_ids = ["sg-12345678"]
-  subnet_id              = "subnet-eddcdzz4"
-
+resource "aws_instance" "web" {
+  ami           = "ami-0427090fd1714168b"       #Amazon Linux AMI
+  instance_type = "t2.micro"
+//  security_groups = [aws_security_group.TF_SG.name] //
+  
   tags = {
-    Terraform   = "true"
-    Environment = "dev"
+    Name = "Terraform Ec2"
   }
 }
