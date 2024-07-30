1b56c9bf56902a33a9197a83e29d1e41a2e1df40
tejashjoshi11 <tejash.joshi@hotmail.com>
2024-07-30 17:10:02 +0530

    Update main.tf

--- EC2_Single/main.tf
+++ EC2_Single/main.tf
@@ -1,9 +1,40 @@
-resource "aws_instance" "web" {
-  ami           = "ami-0427090fd1714168b"       #Amazon Linux AMI
-  instance_type = "t2.micro"
-//  security_groups = [aws_security_group.TF_SG.name] //
+# main.tf
+
+# Specify the AWS provider
+provider "aws" {
+  region = "us-west-2"
+}
+
+# Create a security group to allow SSH access
+resource "aws_security_group" "allow_ssh" {
+  name        = "allow_ssh"
+  description = "Allow SSH inbound traffic"
   
-  tags = {
-    Name = "Terraform Ec2"
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
   }
 }
+
+# Create an EC2 instance
+resource "aws_instance" "example" {
+  ami           = "ami-0427090fd1714168b" # Replace with a valid AMI ID for your region
+  instance_type = "t2.micro"
+  
+  # Attach the security group
+  vpc_security_group_ids = [aws_security_group.allow_ssh.id]
+
+  tags = {
+    Name = "ExampleInstance"
+  }
+}
+
